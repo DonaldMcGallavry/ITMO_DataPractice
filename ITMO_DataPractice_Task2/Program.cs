63354bdb60136b3fbf1829da64881848ba99c3db
N.Dubskiy <167004436+DonaldMcGallavry@users.noreply.github.com>
2025-04-26 22:47:14 +0300

    Add files via upload

---
+++ ITMO_DataPractice_Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ITMO_DataPractice_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load sample data
+            var sampleData = new MLModel1.ModelInput()
+            {
+                Content = @"Hi team, Just a reminder that we have our weekly project status meeting tomorrow at 10 AM in Conference Room B. Please bring your status reports and be prepared to discuss the timeline updates. Thanks, Sarah",
+            };
+
+            //Load model and predict output
+            var result = MLModel1.Predict(sampleData);
+            Console.WriteLine(result.PredictedLabel);
+        }
+    }
+}
